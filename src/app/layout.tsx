3de87ee3cf328fb27a70dbd3e48783a433482b2a
iroro1 <ojigboleo@gmail.com>
2026-02-26 12:09:43 -0600

    initial landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,12 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
+
 export default function RootLayout({
   children,
 }: {
